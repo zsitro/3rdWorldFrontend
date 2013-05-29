98ac9c8d25365e72b2e0c3c4e081893acae87575
Gábor Zsótér <zsitro@gmail.com>
2013-05-29 02:50:56 -0400

    pattern class

---
+++ javascript-vanilla/patterns.var.js
@@ -0,0 +1,13 @@
+(function(){
+
+    "use strict";
+    
+    var i,
+        counter,
+        hasItems = false,
+        isconnected = true,
+        container;
+        
+        /* rest of your code */
+
+})();
